0e0e34dd6ab7f709aa6ab0870373b7afc7e038f5
SergeySalikov <sseerrgey@icloud.com>
2017-10-05 22:51:14 +0300

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j;
 
+/**
+ * Class Calculate для вывода "Hello world".
+ * @author salikov
+ * @since 05.10.2017
+ * @version 1
+ */
+
 class Calculate {
+	/**
+	* Конструктор, вывод строки в консоль.
+	* @param arg - args.
+	*/
 	public static void main(String[] args) {
 		System.out.print("Hello world.");
 	}
