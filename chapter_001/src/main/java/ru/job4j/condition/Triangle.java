4e9ad06df5454b1dceb3fec36b738686ceafe245
SergeySalikov <sseerrgey@icloud.com>
2017-10-14 01:56:19 +0300

    task 9461

---
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -0,0 +1,95 @@
+package ru.job4j.condition;
+
+/**
+ * Class Triangle.
+ *
+ * @author Salikov
+ * @version 1
+ * @since 13.10.2017
+ */
+
+public class Triangle {
+    /**
+     * точка a.
+     */
+    private Point a;
+    /**
+     * точка b.
+     */
+    private Point b;
+    /**
+     * точка c.
+     */
+    private Point c;
+
+    /**
+     * Конструктор - создание нового объекта с определенными значениями.
+     *
+     * @param a - точка a.
+     * @param b - точка b.
+     * @param c - точка c.
+     */
+    public Triangle(Point a, Point b, Point c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    /**
+     * Метод вычисляет расстояние между точками.
+     *
+     * @param left  - первая точка.
+     * @param right - вторая точка.
+     * @return возвращает значение расстояния между точками.
+     */
+    public double distance(Point left, Point right) {
+        double result = Math.sqrt(Math.pow((right.getX() - left.getX()), 2) + Math.pow((right.getY() - left.getY()), 2));
+        return result;
+    }
+
+    /**
+     * Метод вычисляет периметр треугольника.
+     *
+     * @param ab - расстояние между точками a и b
+     * @param ac - расстояние между точками a и c
+     * @param bc - расстояние между точками b и c
+     * @return возвращает значение расстояния между точками.
+     */
+    public double period(double ab, double ac, double bc) {
+        double perimeter = (ab + ac + bc) / 2;
+        return perimeter;
+    }
+
+    /**
+     * Метод вычисляет площадь треугольника.
+     *
+     * @return возвращает значение площади треугольника.
+     */
+    public double area() {
+        double rsl = -1;
+        double ab = this.distance(this.a, this.b);
+        double ac = this.distance(this.a, this.c);
+        double bc = this.distance(this.b, this.c);
+        double p = this.period(ab, ac, bc);
+        if (this.exist(ab, ac, bc)) {
+            rsl = Math.sqrt(p * (p - ab) * (p - ac) * (p - bc));
+        }
+        return rsl;
+    }
+
+    /**
+     * Метод проверяет возможность построения треугольника.
+     *
+     * @param ab - расстояние между точками a и b
+     * @param ac - расстояние между точками a и c
+     * @param bc - расстояние между точками b и c
+     * @return возвращает true если возможно, иначе false.
+     */
+    private boolean exist(double ab, double ac, double bc) {
+        boolean result = true;
+        if (ab == 0 || ac == 0 || bc == 0) {
+            result = false;
+        }
+        return result;
+    }
+}
