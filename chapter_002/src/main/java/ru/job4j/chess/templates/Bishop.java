c035407bd87edfaef70b97fb2fc37c1abd2f4cf6
SergeySalikov <sseerrgey@icloud.com>
2018-01-16 23:42:07 +0300

    [#792]

--- chapter_002/src/main/java/ru/job4j/chess/templates/Bishop.java
+++ chapter_002/src/main/java/ru/job4j/chess/templates/Bishop.java
@@ -10,24 +10,17 @@ public class Bishop extends Figure {
     }
 
     public Cell[] way(Cell source, Cell dest) throws ImpossibleMoveException {
-        int numberCell = Math.abs(source.getY() - dest.getY());
-        Cell[] cell = new Cell[numberCell];
+        int deltaX = source.getX() - dest.getX();
+        int deltaY = source.getY() - dest.getY();
+        int size = Math.abs(deltaY);
+        Cell[] cell = new Cell[size];
         if (dest.getX() <= 0 || dest.getY() <= 0 || dest.getX() == source.getX() || dest.getY() == source.getY() || dest.getX() > 8 || dest.getY() > 8)
             throw new ImpossibleMoveException("(1)не возможен ход в клетку:", dest);
-        for (int i = 1; i <= numberCell; i++) {
-            if (source.getX() < dest.getX() && source.getY() < dest.getY()) {
-                cell[i - 1] = new Cell(source.getX() + i, source.getY() + i); //юго-восток
-            } else if (source.getX() > dest.getX() && source.getY() > dest.getY()) {
-                cell[i - 1] = new Cell(source.getX() - i, source.getY() - i); //северо-запад
-            } else if (source.getX() < dest.getX() && source.getY() > dest.getY()) {
-                cell[i - 1] = new Cell(source.getX() + i, source.getY() - i); //северо-восток
-            } else if (source.getX() > dest.getX() && source.getY() < dest.getY()) {
-                cell[i - 1] = new Cell(source.getX() - i, source.getY() + i); //юго-запад
-            }
+        for (int i = 0; i < size; i++) {
+            cell[i] = new Cell(source.getX() + (deltaX < 0 ? 1 : -1) * (i + 1), source.getY() + (deltaY < 0 ? 1 : -1) * (i + 1));
         }
-        if (cell[numberCell - 1].getX() != dest.getX() || cell[numberCell - 1].getY() != dest.getY())
+        if (cell[size - 1].getX() != dest.getX() || cell[size - 1].getY() != dest.getY())
             throw new ImpossibleMoveException("(2)не возможен ход в клетку:", dest);
-
         return cell;
     }
 }
