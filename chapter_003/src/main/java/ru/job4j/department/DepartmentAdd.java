bfa30b9be25a770f6b14dda19817240df9382020
SergeySalikov <sseerrgey@icloud.com>
2018-03-12 18:48:18 +0300

    Отсортировать департаменты

--- chapter_003/src/main/java/ru/job4j/department/DepartmentAdd.java
+++ chapter_003/src/main/java/ru/job4j/department/DepartmentAdd.java
@@ -1,29 +1,18 @@
 package ru.job4j.department;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.TreeSet;
 
 
 public class DepartmentAdd {
-    public void addMissing(ArrayList<String> arr) {
-        for (int out = 0; out < arr.size(); out++) {
-            if (arr.get(out).length() >= 11) {
-                if (Collections.frequency(arr, arr.get(out).substring(0, arr.get(out).indexOf("\\"))) < 1) {
-                    arr.add(arr.get(out).substring(0, arr.get(out).indexOf("\\")));
-                } else if (Collections.frequency(arr, arr.get(out).substring(0, arr.get(out).indexOf("\\", 5))) < 1) {
-                    arr.add(arr.get(out).substring(0, arr.get(out).indexOf("\\", 5)));
-                }
-            }
-
-            if (arr.get(out).length() > 5 && arr.get(out).length() < 9) {
-                if (Collections.frequency(arr, arr.get(out).substring(0, arr.get(out).indexOf("\\"))) < 1) {
-                    arr.add(arr.get(out).substring(0, arr.get(out).indexOf("\\")));
-                }
-            }
-
+    public TreeSet<String> addMissing(ArrayList<String> arr) {
+        TreeSet<String> data = new TreeSet<>();
+        data.addAll(arr);
+        for (String temp : arr) {
+            data.add(temp.substring(0, temp.lastIndexOf('\\')));
+            data.add(temp.substring(0, temp.indexOf('\\')));
 
         }
-
-
+        return data;
     }
 }
